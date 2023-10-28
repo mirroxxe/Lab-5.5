f7c05a53dddcdc5f6bf99aa7fa0eea08b8e550a6
Volodya <chuchman002@gmail.com>
2023-10-28 12:14:02 +0300

    Add project files.

---
+++ Lab №5.5/Lab №5.5.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+int A(int m, int n, int level, int& depth) {
+    if (level > depth)
+        depth = level;
+    cout << "Level: " << level << endl;
+
+    if (m == 0) {
+        return n + 1;
+    }
+    else if (n == 0) {
+        return A(m - 1, 1, level + 1, depth);
+    }
+    else {
+        return A(m - 1, A(m, n - 1, level + 1, depth), level + 1, depth);
+    }
+}
+
+int main() {
+    int m, n;
+    std::cout << "Two positive numbers: \n";
+    std::cin >> m >> n;
+    int depth = 0; // глибина рекурсії
+    int result = A(m, n, 1, depth); 
+    std::cout << "Result: " << result << std::endl;
+    std::cout << "Rec depth: " << depth << std::endl; // вивід глибини рекурсії
+    return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab №5.5/Lab №5.5.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		TEST_METHOD(TestAFunction)
+		{
+			int depth = 0;
+			int result = A(2, 3, 1, depth);
+
+			Assert::AreEqual(result, 9);
+			Assert::AreEqual(depth, 10);
+		}
+	};
+}
